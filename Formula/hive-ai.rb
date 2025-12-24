143b3dc1879e4e660aae7a84990bbda42300f137
mathieu.bourmaud <mathieu.bourmaud@emeria.eu>
2025-12-24 11:20:49 +0100

    chore: update hive-ai to v1.0.2

--- Formula/hive-ai.rb
+++ Formula/hive-ai.rb
@@ -1,35 +1,40 @@
 class HiveAi < Formula
   desc "Multi-Agent Claude System for parallel development"
   homepage "https://github.com/mbourmaud/hive"
-  version "0.2.0"
+  version "1.0.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mbourmaud/hive/releases/download/v0.2.0/hive-darwin-arm64.tar.gz"
-      sha256 "8ba9f741df85ea2125e4eb97f3d5dfeac8d541bac34f6e207697477c7c342c17"
+      url "https://github.com/mbourmaud/hive/releases/download/v1.0.2/hive-darwin-arm64.tar.gz"
+      sha256 "3b1e12533d9efe9e8400113ee85c6d58b512bd9eff4ac519272c51d47c829588"
     else
-      url "https://github.com/mbourmaud/hive/releases/download/v0.2.0/hive-darwin-amd64.tar.gz"
-      sha256 "0a20174a2b1aba62bb3a2295e66cc662879e43988a2fbdaaa839c234ecc963b2"
+      url "https://github.com/mbourmaud/hive/releases/download/v1.0.2/hive-darwin-amd64.tar.gz"
+      sha256 "d0edbd2b9e3574e7545fa2667bf8e58e90d507a2c31f4e950e68038995b020f5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/mbourmaud/hive/releases/download/v0.2.0/hive-linux-arm64.tar.gz"
-      sha256 "5a67a8dac65a6d0a40d20d016c6a2f9da73f01c7d7488f1bd4b86e4786475b3f"
+      url "https://github.com/mbourmaud/hive/releases/download/v1.0.2/hive-linux-arm64.tar.gz"
+      sha256 "1164c211ae3650002c3b2b595ee01d211ca130c7d42d91b90a7345b624cea343"
     else
-      url "https://github.com/mbourmaud/hive/releases/download/v0.2.0/hive-linux-amd64.tar.gz"
-      sha256 "d36008c2753821e81d3758f428f51b83889d84b722cd4f10b3bca38bc6a221b6"
+      url "https://github.com/mbourmaud/hive/releases/download/v1.0.2/hive-linux-amd64.tar.gz"
+      sha256 "70c406e766b6ce935dbbf8aba10f8dbcedcddcbd9838b1fbacdeee224ab0b657"
     end
   end
 
   depends_on "docker" => :recommended
 
   def install
-    bin.install Dir["hive-*"].first => "hive"
+    # The tarball contains a binary named hive-{os}-{arch}
+    os = OS.mac? ? "darwin" : "linux"
+    arch = Hardware::CPU.arm? ? "arm64" : "amd64"
+    binary_name = "hive-#{os}-#{arch}"
+    
+    bin.install binary_name => "hive"
   end
 
   test do
-    assert_match "hive", shell_output("#{bin}/hive --help")
+    assert_match version.to_s, shell_output("#{bin}/hive --version")
   end
 end
